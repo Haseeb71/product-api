95dd2b52a3a4a4546eeeb9debbbf9ae63e20869d
Abdulhaseeb711 <108294099+Abdulhaseeb711@users.noreply.github.com>
2022-09-12 04:20:36 -0700

    pull request

--- productapp/controller/productController.js
+++ productapp/controller/productController.js
@@ -3,18 +3,20 @@ const mongoose = require("mongoose");
 const Categories = require("../modal/productCategorie");
 const Products = require("../modal/products");
 
+// To Show category Page 
 const index = async (req, res) => {
   const found = await Categories.find({});
   res.send(found);
 };
-const product = async (req, res) => {
 
+// To show all products of specific Category
+const product = async (req, res) => {
    const found = await Products.find({}).where('categoryId').equals(req.params.id);
   res.send(found);
 };
 
+// To Show individual Product details
 const show = async (req, res) => {
-
   const found = await Products.findById(req.params.id);
   res.send(found);
 }
